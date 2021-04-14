3edba0bfcb69a926c167fa2b2763c8b4c665c1d2
maxep <me@maxep.me>
2021-04-14 12:48:12 +0200

    Remove react-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
-import { Helmet } from "react-helmet";
 import { useUserQuery } from "./github/types";
 import Container from "./components/container";
 import Header from "./components/header";
@@ -10,7 +9,6 @@ import theme from "./theme";
 import config from "./config.json";
 
 type Config = {
-  name: string;
   username: string;
   description: {
     title: string;
@@ -21,7 +19,7 @@ type Config = {
 } & ApplicationsProps;
 
 const App = (): React.ReactElement => {
-  const { name, username, description, applications } = config as Config;
+  const { username, description, applications } = config as Config;
 
   const { data } = useUserQuery({
     variables: { user: username },
@@ -30,15 +28,6 @@ const App = (): React.ReactElement => {
   return (
     <ChakraProvider theme={theme}>
       <CSSReset />
-      <Helmet>
-        <title>{name}</title>
-        <meta name={name} content={description.title} />
-        <meta name="description" content={description.message} />
-        <meta property="og:title" content={description.title} />
-        <meta property="og:description" content={description.message} />
-        <meta property="og:image" content={data?.user?.avatarUrl} />
-        <link rel="apple-touch-icon" href={data?.user?.avatarUrl} />
-      </Helmet>
       <Container>
         <Header data={data} {...description} />
         <Applications applications={applications} />
